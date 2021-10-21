39b48290a4ecc758f3705be629a699cefba88a97
shantkalaidjian <shant.kalaidjian@student.hu.nl>
2021-10-21 10:08:06 +0200

    First Commit ever

---
+++ src/main/java/com/luv2code/ecommerce/dao/ProductRepository.java
@@ -0,0 +1,9 @@
+package com.luv2code.ecommerce.dao;
+
+import com.luv2code.ecommerce.entity.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+@CrossOrigin(origins = "http://localhost:4200")
+public interface ProductRepository extends JpaRepository<Product, Long> {
+}
